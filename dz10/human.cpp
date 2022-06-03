799b970a72a5a9a3487c51c299f1573a1026ca76
mishnevo <pepefrog240@gmail.com>
2022-06-03 23:35:37 +0300

    Добавьте файлы проекта.

---
+++ dz10/human.cpp
@@ -0,0 +1,8 @@
+#include "human.h"
+int main() {
+
+	Student Steve("Steve_one", "Steve_two", "Steve_three", 17, 1);
+	Boss Mark("Mark_one", "Mark_two", "Mark_three", 25, 10);
+	Steve.print();
+	Mark.print();
+}
